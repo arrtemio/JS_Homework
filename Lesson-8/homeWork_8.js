4d6d84e9771998757cbfe6f2e1ad703c9ba2f2c6
Artsiom Hardziyonak <arrtemio@gmail.com>
2022-06-26 22:11:52 +0300

    Homework 8

---
+++ Lesson-8/homeWork_8.js
@@ -0,0 +1,44 @@
+"use strict";
+
+function Animal() {
+  var self = this;
+  this._foodAmount = 50;
+  this.name = name;
+
+  this.formatFoodAmount = function () {
+    return this._foodAmount + " гр.";
+  };
+
+  self.dailyNorm = function (amount) {
+    if (!arguments.length) return this.formatFoodAmount();
+
+    if (amount < 30 || amount > 100) {
+      return "Недопустимое количество корма.";
+    }
+
+    this._foodAmount = amount;
+  };
+
+  self.feed = function () {
+    console.log("Насыпаем в миску " + self.dailyNorm() + " корма.");
+  };
+
+  this.stroke = function () {
+    console.log("Гладим кота");
+    return this;
+  };
+}
+
+function Cat(name) {
+  Animal.apply(this, arguments);
+
+  var animalFeed = this.feed;
+
+  this.feed = function () {
+    animalFeed();
+    console.log("Кот доволен ^_^");
+    return this;
+  };
+}
+
+var barsik = new Cat("Барсик");
