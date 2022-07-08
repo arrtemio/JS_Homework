98f6ad0639b390712f9c14447603405d58571e2c
Artsiom Hardziyonak <arrtemio@gmail.com>
2022-07-08 18:43:55 +0300

    Homework 11

---
+++ Lesson-11/myHomework_11.js
@@ -0,0 +1,78 @@
+// Задание 1:
+
+"use strict";
+
+var nameArr = ["Vasiliy", "Pavel", "Alex"];
+
+function arrObj(arr) {
+  var objName = [];
+
+  var nameObj = arr.map(function (names) {
+    return objName.push({ name: names });
+  });
+
+  return objName;
+}
+
+arrObj(nameArr);
+
+// Задание 2:
+
+var currentArr = ["00", "13", "24"];
+
+function arrToTime(arr) {
+  var time = "Текущее время";
+
+  var currentTime = arr.reduce(function (prev, item) {
+    return prev + " : " + item;
+  }, time);
+
+  return currentTime;
+}
+
+arrToTime(currentArr);
+
+// Задание 3:
+
+var text = prompt("Введите предложение"); // 15
+
+function getVowels(text) {
+  var vowels = ["а","у","о","ы","э","е","ё","и","ю","я","a","e","i","o","u","y"];
+  var newArr = [];
+  var textArr = text.toLowerCase().split("");
+
+  textArr.map(function (letter) {
+    vowels.find(function (vowelsLetter) {
+      if (letter === vowelsLetter) {
+        return newArr.push(letter);
+      }
+    });
+  });
+
+  return newArr.length;
+}
+
+getVowels(text);
+
+// Задание 4:
+
+var text = prompt('Введите ваш текст');
+
+function countSentencesLetters(someText) {
+    var arr = [];
+    var textArr = someText.split(/[.?!]/);
+  
+    for (var k in textArr) {
+      if (textArr[k]) {
+        arr.push(textArr[k]);
+      }
+    }
+  
+    arr.forEach(function (item) {
+      var lettersLength = item.trim().split(/[" ",]/).join('').length;
+  
+      return console.log(item.trim() + " : Letters quantity is: " + lettersLength);
+    });  
+}
+
+countSentencesLetters(text);
