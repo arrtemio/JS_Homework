11d531f7acf4d9d1f0663b34c528d9cf193247d1
Artsiom Hardziyonak <arrtemio@gmail.com>
2022-07-30 18:59:41 +0300

    Homework 15

--- Lesson-15/script.js
+++ Lesson-15/script.js
@@ -1,34 +1,70 @@
-// Задание 1:
-// Добавить на страницу кнопку "Загрузить список пользователей".
-// По клику на нее осуществляется AJAX (GET) запрос на https://reqres.in (List Users).
-// В случае успешного получения данных:
-//   - ДИНАМИЧЕСКИ добавить на страницу вкладки "Пользователь 1", "Пользователь 2" и т.д.
-//   (в соотв. с количеством пользователей)
-//   - сделать первую вкладку активной и показать под ней блок с информацией о пользователе
-//   - вкладки должны иметь возможность переключаться и отображать соответствующие данные
-//   - полученные данные должны записываться в LocalStorage *
-//   - по клику на кнопку должна происходить проверка, если данные есть в LocalStorage, не отправлять AJAX запрос, а
-//     сразу отрисовывать их *
-// В случае ошибки получения данных (протестировать можно, изменив url запроса как в примере с урока):
-//   - отрисовать на странице сообщение о том, что данные не получены, в произвольном виде
-// Перед отправкой ОБЯЗАТЕЛЬНО (!!!) порефакторить код и протестировать ваше приложение на работоспособность и
-// максимально на отсутствие багов.
-
 var xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://reqres.in/api/users?page=2');
 
 xhr.send();
 
-xhr.onload = function () {
-  var statusType = +String(this.status)[0];
-  if (statusType === 2) {
-  }
-};
-
-
 var getUsers = document.getElementById('button');
+var main = document.getElementsByClassName('main')[0];
+var box = document.createElement('div');
 
-getUsers.addEventListener('click', function() {
-    
-})
+getUsers.addEventListener('click', function () {
+  var statusType = +String(xhr.status)[0];
+
+  if (statusType === 2) {
+    var users = JSON.parse(xhr.response).data;
+
+    box.classList.add('box');
+    main.append(box);
+
+    for (var k in users) {
+      var userLink = document.createElement('div');
+
+      userLink.classList.add('user-link');
+      userLink.innerHTML = 'User ' + (users.indexOf(users[k]) + 1);
+      box.append(userLink);
+
+      var userBox = document.createElement('div');
+
+      userBox.classList.add('user-box');
+
+      userBox.innerHTML +=
+        '<img src = "' + users[k].avatar + '">' +
+        '<p>' + 'First Name: ' + users[k].first_name + '<br>' +
+        'Last Name: ' + users[k].last_name + '</p>';
+
+      main.append(userBox);
+    }
+
+    document.getElementsByClassName('user-box')[0].classList.add('show');
+    document.getElementsByClassName('user-link')[0].classList.add('selected-link');
+  } else {
+    main.innerHTML = 'Ошибка обработки данных с сервера';
+  }
+
+  getUsers.disabled = true;
+});
+
+box.addEventListener('click', function (evt) {
+  var target = evt.target;
+
+  if (target.classList.value === 'user-link') {
+    var links = document.getElementsByClassName('user-link');
+    var userBox = document.getElementsByClassName('user-box');
+
+    document.getElementsByClassName('show')[0].classList.remove('show');
+    links[0].classList.remove('selected-link');
+
+    for (var i = 0; i < links.length; i++) {
+      if (target === links[i]) {
+        links[i].classList.add('selected-link');
+        userBox[i].classList.add('show');
+      } else {
+        if (links[i] !== target) {
+          links[i].classList.remove('selected-link');
+          userBox[i].classList.remove('show');
+        }
+      }
+    }
+  }
+});
